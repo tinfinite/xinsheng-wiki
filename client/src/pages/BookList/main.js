ea44d264a35736c9dca323812c88d9f2869bf39d
zjxgit <xiang198976@gmail.com>
2017-05-04 14:53:01 +0800

    js打包

--- client/src/pages/BookList/main.js
+++ client/src/pages/BookList/main.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#list',
   template: '<App/>',
   components: { App }
 })
--- client/src/pages/WikiConfig/main.js
+++ client/src/pages/WikiConfig/main.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#config',
   template: '<App/>',
   components: { App }
 })
